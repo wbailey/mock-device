9e14dd81f9712b2947f336118570b3056a7c32f9
Wes Bailey <wes@sproutling.com>
2017-01-20 00:04:26 -0800

    stubbing out command line interface

--- main.go
+++ main.go
@@ -2,15 +2,23 @@ package main
 
 import "fmt"
 import "github.com/wbailey/emitters"
+import "github.com/urfave/cli"
 import "time"
+import "os"
 
 //import "flag"
 
 func main() {
+	app := cli.NewApp()
+	app.Name = "mock-device"
+	app.Usage = "simulate an IoT device"
+
 	c := emitters.ConstantEmitter{1.0, 1}
 
 	for i := 0; i < 10; i++ {
 		fmt.Println(i, c.Emit())
 		time.Sleep(c.UseFreq())
 	}
+
+	app.Run(os.Args)
 }
